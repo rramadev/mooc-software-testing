74b0539da859c10eadb7e8b95360f7025dac2fcc
Raúl <Raul@samsung-pc.com>
2021-01-25 15:00:47 +0100

    Improves gHappy tests

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -6,8 +6,13 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 public class GHappyTest {
   @ParameterizedTest
-  @CsvSource({ "'xgx', false", "'xg', false", "'gx', false", "'g', false", 
-               "'xggx', true", "'ggx', true", "'xgg', true", "'gg', true"})
+  @CsvSource({ // Unhappy Gs
+               "'xgx', false", "'xg', false", "'gx', false", "'g', false",
+               "'xggxgx', false", "'xgggxgx', false",
+               // Happy Gs
+               "'xggx', true", "'ggx', true", "'xgg', true", "'gg', true",               
+               "'xggxggx', true", "'xgggx', true", "'ggg', true",
+               "'', true"})
   public void isGHappy(String str, boolean expected) {
       GHappy gHappy = new GHappy();
       Assertions.assertEquals(expected, gHappy.gHappy(str));
