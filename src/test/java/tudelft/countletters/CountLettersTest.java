79bd0ef60480dc2b6e16594b1421d552914bf690
Raúl <Raul@samsung-pc.com>
2021-01-26 12:27:35 +0100

    Update Tests with MCDC covering

--- src/test/java/tudelft/countletters/CountLettersTest.java
+++ src/test/java/tudelft/countletters/CountLettersTest.java
@@ -7,14 +7,30 @@ public class CountLettersTest {
 
     @Test
     public void multipleMatchingWords() {
-        int words = new CountLetters().count("cats|dogs");
+        CountLetters counter = new CountLetters();
+        
+        int words = counter.count("cats|dogs");
+        Assertions.assertEquals(2, words);
+
+        words = counter.count("catr|dogr");
         Assertions.assertEquals(2, words);
     }
 
+    @Test
+    public void lastWordBeforeNoLetterDoesNotMatch() {
+        int words = new CountLetters().count("cat|dogs");
+        Assertions.assertEquals(1, words);
+    }
+
+    @Test
+    public void matchWordDoesNotCount() {
+        int words = new CountLetters().count("cardogs");
+        Assertions.assertEquals(1, words);
+    }
+    
     @Test
     public void lastWordDoesNotMatch() {
         int words = new CountLetters().count("cats|dog");
         Assertions.assertEquals(1, words);
     }
-
 }
