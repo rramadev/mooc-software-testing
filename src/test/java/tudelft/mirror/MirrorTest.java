1b1cef2bc41cbe73060426bc667608d6486f901e
Raúl <Raul@samsung-pc.com>
2021-01-28 15:15:15 +0100

    Add tests to mirror

--- src/test/java/tudelft/mirror/MirrorTest.java
+++ src/test/java/tudelft/mirror/MirrorTest.java
@@ -1,4 +1,43 @@
 package tudelft.mirror;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class MirrorTest {
+
+  private Mirror mirror;
+
+  @BeforeEach
+    public void initialize() {
+        this.mirror = new Mirror();
+    }
+
+  @Test
+  public void hasEmptyMirror() {
+    String result = mirror.mirrorEnds("abcd");
+
+    Assertions.assertEquals("", result);
+  }
+
+  @Test
+  public void hasOneLetterMirror() {
+    String result = mirror.mirrorEnds("abca");
+
+    Assertions.assertEquals("a", result);
+  }
+
+  @Test
+  public void hasMoreThanOneLetterMirror() {
+    String result = mirror.mirrorEnds("abCDXba");
+
+    Assertions.assertEquals("ab", result);
+  }
+
+  @Test
+  public void hasOverlappingMirror() {
+    String result = mirror.mirrorEnds("abcba");
+
+    Assertions.assertEquals("abcba", result);
+  }
 }
